91c664f967565b3b2de38d3deda062e2a4d9e35d
shana <shanaw1999@gmail.com>
2020-09-09 13:06:56 -0400

    create move method test

--- src/test/java/touro/snake/SnakeTest.java
+++ src/test/java/touro/snake/SnakeTest.java
@@ -2,7 +2,11 @@ package touro.snake;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 public class SnakeTest {
 
@@ -13,7 +17,25 @@ public class SnakeTest {
 
     @Test
     public void move() {
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+
+        //if
+        Direction direction = Direction.North;
+        SnakeHeadStateMachine state = new SnakeHeadStateMachine(direction);
+        Snake snake = new Snake(state);
+        //FoodFactory foodFactory = mock(FoodFactory.class);
+        //Garden garden = new Garden(snake, foodFactory);
+        List<Square> previousSquares = new ArrayList<>(snake.getSquares());
+
+        //when
+        snake.move();
+
+        //then
+        List<Square> currentSquares = snake.getSquares();
+        assertEquals(previousSquares.get(0).getY() - 1, currentSquares.get(0).getY());
+        for(int i = 1; i < currentSquares.size(); i++) {
+            assertEquals(previousSquares.get(i - 1).getY(), currentSquares.get(i).getY());
+        }
+
     }
 
     @Test
