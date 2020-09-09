4d3158ae965753d8c66b9449949600e6dc69078a
Ricki Peikes <rickipeikes@gmail.com>
2020-09-08 22:25:01 -0400

    implemented getInstanceOf method in FoodFactory

--- src/main/java/touro/snake/FoodFactory.java
+++ src/main/java/touro/snake/FoodFactory.java
@@ -10,9 +10,15 @@ public class FoodFactory {
     /**
      * @return a new Food with random coordinates in the Garden
      */
+    private final int GARDEN_WIDTH = 100;
+    private final int GARDEN_HEIGHT = 40;
+
     public Food newInstance() {
         // Peikes
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+        Random coordinateGenerator = new Random();
+        int randX = coordinateGenerator.nextInt(GARDEN_WIDTH)+1;
+        int randY = coordinateGenerator.nextInt(GARDEN_HEIGHT)+1;
+        return new Food(randX,randY);
     }
 
 }
