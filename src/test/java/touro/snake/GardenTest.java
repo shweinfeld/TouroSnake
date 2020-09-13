abd7408fc8ef5d3240b627586bd292775aab094a
Devora Nagel <devoranagel@gmail.com>
2020-09-13 13:45:25 -0500

    added test for createFoodIfNecessary

--- src/test/java/touro/snake/GardenTest.java
+++ src/test/java/touro/snake/GardenTest.java
@@ -2,6 +2,8 @@ package touro.snake;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class GardenTest {
@@ -13,6 +15,23 @@ public class GardenTest {
 
     @Test
     public void createFoodIfNecessary() {
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+        
+        //if
+        SnakeHeadStateMachine state = new SnakeHeadStateMachine(Direction.North);
+        Snake snake = new Snake(state);
+        FoodFactory foodFactory = new FoodFactory();
+        Garden garden = new Garden(snake, foodFactory);
+        List<Square> snakeBody = snake.getSquares();
+        
+        //when
+        garden.createFoodIfNecessary();
+        
+        //then
+        int x = foodFactory.newInstance().getX();
+        int y = foodFactory.newInstance().getY();
+
+        Square square = new Square(x, y);
+
+        assertNotEquals(snakeBody, square);
     }
 }
