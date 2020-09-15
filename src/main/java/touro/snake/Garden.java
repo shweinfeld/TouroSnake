fa11f8ec22aa851642d1bb44f1c9da7945bcda42
Devora Nagel <devoranagel@gmail.com>
2020-09-14 22:22:01 -0500

    removed unnecessary imports

--- src/main/java/touro/snake/Garden.java
+++ src/main/java/touro/snake/Garden.java
@@ -1,6 +1,5 @@
 package touro.snake;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
--- src/test/java/touro/snake/GardenTest.java
+++ src/test/java/touro/snake/GardenTest.java
@@ -1,7 +1,6 @@
 package touro.snake;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.util.List;
 
