a939b73ebd2b75e322e869e2ce2a1f784ad2d82d
jkomendant <jennifer.komendant@gmail.com>
2020-09-15 11:01:52 -0400

    removed unneeded imports

--- src/main/java/touro/snake/SnakeFrame.java
+++ src/main/java/touro/snake/SnakeFrame.java
@@ -2,9 +2,7 @@ package touro.snake;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Scanner;
 
 public class SnakeFrame extends JFrame {
 
