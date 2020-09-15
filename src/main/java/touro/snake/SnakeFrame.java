85bf16138011ea6c0bdd16b7df9bb52a63459f92
jkomendant <jennifer.komendant@gmail.com>
2020-09-14 23:15:25 -0400

    created frame that calls upon gardenview and SnakeKeyListener classes

--- src/main/java/touro/snake/SnakeFrame.java
+++ src/main/java/touro/snake/SnakeFrame.java
@@ -2,16 +2,25 @@ package touro.snake;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class SnakeFrame extends JFrame {
+
+    KeyEvent e;
+
     public SnakeFrame(
             GardenView gardenView,
             SnakeKeyListener snakeKeyListener
     ) throws HeadlessException {
         super();
 
-        // Komendant
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+        setSize(800,600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Snake");
+        setLayout(new BorderLayout());
+
+        add(gardenView, BorderLayout.CENTER);
+        snakeKeyListener.keyPressed(e);
 
     }
 }
