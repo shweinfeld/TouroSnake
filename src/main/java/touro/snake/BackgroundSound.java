232460ef4ac852835263b6d3807be1f9204c1f34
jkomendant <jennifer.komendant@gmail.com>
2020-09-22 18:41:04 -0400

    moved wav file into resources under main

--- src/main/java/touro/snake/BackgroundSound.java
+++ src/main/java/touro/snake/BackgroundSound.java
@@ -11,7 +11,7 @@ public class BackgroundSound {
 
         try {
 
-            File musicPath = new File("src/test/resources/TheSpectre.wav");
+            File musicPath = new File("src/main/resources/TheSpectre.wav");
 
             AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
             Clip clip = AudioSystem.getClip();
