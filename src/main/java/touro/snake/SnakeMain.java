3d3f759b2b639b4f4d0fc691a5d514b5e5365710
SaraOrlian <orlian.sara@gmail.com>
2020-09-21 21:34:36 -0400

    added .wav file as a parameter to Garden object

--- src/main/java/touro/snake/SnakeMain.java
+++ src/main/java/touro/snake/SnakeMain.java
@@ -11,7 +11,7 @@ public class SnakeMain {
         SnakeHeadStateMachine snakeHeadStateMachine = new SnakeHeadStateMachine(Direction.West);
         Snake snake = new Snake(snakeHeadStateMachine);
         FoodFactory foodFactory = new FoodFactory();
-        Garden garden = new Garden(snake, foodFactory);
+        Garden garden = new Garden(snake, foodFactory, "EatNoise.wav");
         GardenView gardenView = new GardenView(garden);
         SnakeKeyListener snakeKeyListener = new SnakeKeyListener(snake);
 
