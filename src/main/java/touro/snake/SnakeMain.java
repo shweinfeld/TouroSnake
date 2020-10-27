58f72a3a91dcf26de67deb09fc9cde4a31020001
shana <shanaw1999@gmail.com>
2020-10-27 18:44:42 -0400

    Revert "remove changes made in main"
    This reverts commit b2a29973aae2c56e32a4646518d01e1761102d54.
    
    "revert commit"

--- src/main/java/touro/snake/SnakeMain.java
+++ src/main/java/touro/snake/SnakeMain.java
@@ -1,6 +1,8 @@
 package touro.snake;
 
-import touro.snake.strategy.BlankStrategy;
+import touro.snake.strategy.DirectionStrategy;
+import touro.snake.strategy.astar.weinfeld.AStarStrategy;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -13,7 +15,7 @@ public class SnakeMain {
         // Set up all class dependencies here.
 
         SnakeHeadStateMachine snakeHeadStateMachine = new SnakeHeadStateMachine(Direction.West);
-        Snake snake = new Snake(snakeHeadStateMachine, new BlankStrategy());
+        Snake snake = new Snake(snakeHeadStateMachine, new AStarStrategy());
         FoodFactory foodFactory = new FoodFactory();
         try {
             InputStream inputStream = Garden.class.getClassLoader().getResourceAsStream("EatNoise.wav");
