3f5a0714b62b05f1968df806d5482603834c3440
shana <shanaw1999@gmail.com>
2020-10-27 18:48:07 -0400

    remove changes made in main

--- src/main/java/touro/snake/SnakeMain.java
+++ src/main/java/touro/snake/SnakeMain.java
@@ -1,8 +1,6 @@
 package touro.snake;
 
-import touro.snake.strategy.DirectionStrategy;
-import touro.snake.strategy.astar.weinfeld.AStarStrategy;
-
+import touro.snake.strategy.BlankStrategy;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -15,7 +13,7 @@ public class SnakeMain {
         // Set up all class dependencies here.
 
         SnakeHeadStateMachine snakeHeadStateMachine = new SnakeHeadStateMachine(Direction.West);
-        Snake snake = new Snake(snakeHeadStateMachine, new AStarStrategy());
+        Snake snake = new Snake(snakeHeadStateMachine, new BlankStrategy());
         FoodFactory foodFactory = new FoodFactory();
         try {
             InputStream inputStream = Garden.class.getClassLoader().getResourceAsStream("EatNoise.wav");
